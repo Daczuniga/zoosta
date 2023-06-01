3612ea29d5974a118c803baa217cccea0df400e2
Marxho <marxhop@hotmail.com>
2023-06-01 04:11:29 -0400

    create routes for pages, update client-pages and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Navbar from './components/Navbar';
 function App() {
   return (
-   <main>
-   <header>
-   </header>
-   <div className="navBar"> 
-      <a className="signUp" href="">Sign Up</a>
-      <a className="login" href="">Login</a> 
-    </div>
-   </main>
+   <div className="App">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/Home" element={<Home/>}/>
+          <Route path="/Login" element={<Login/>}/>
+          <Route path="/SignUp" element={<SignUp/>}/>
+        </Routes>
+      </Router>
+   </div>
   );
 }
 
 export default App;
+
 
