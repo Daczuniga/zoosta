350a9f8630d9a670085155e25961f9bab166ac5d
Daniel <danielcruz.34776@gmail.com>
2023-05-31 21:24:13 -0400

    Revised index.js Routes

--- zoosta/server/routes/index.js
+++ zoosta/server/routes/index.js
@@ -1,8 +1,23 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-router.use('/api', apiRoutes);
+const cwd = process.cwd();
 
-router.use((req, res) => res.send('Wrong route!'));
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-module.exports = router;
+// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
+const activity = cwd.includes('01-Activities')
+  ? cwd.split('/01-Activities/')[1]
+  : cwd;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
