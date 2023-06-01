8c650d388c66d0b93564d8c49370d7a6d6598c4c
Marxho <marxhop@hotmail.com>
2023-06-01 18:48:32 -0400

    update logo and styles

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../styles/Home.css';
+import Logo from '../assets/zoosta2.png';
 
 function Home() {
     return (
-        <div>
-            <div className='header'>
-            </div>
-            <div className="navBar"> 
+        <div className="main">
+        <div className='Logo'>
+            <img src={Logo} alt="logo"/>
+        </div>
+        <div className="logBar"> 
                 <a className="signup" href="/SignUp">Sign Up</a>
                 <a className="login" href="Login">Login</a> 
             </div>
