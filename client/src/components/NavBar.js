0f13cccab2e9310b4486cc90fe5862f60fb89e9c
Aivi <olivianitzel@gmail.com>
2023-06-05 11:43:48 -0400

    fixed errors added footer

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import '../styles/Navbar.css';
+import '../styles/NavBar.css';
 import ReorderIcon from '@material-ui/icons/Reorder';
 
 function Navbar() {
