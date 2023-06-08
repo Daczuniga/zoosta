0f7a05afa29b4e432a53e13753e8d778faafa0f7
aya effoua <carineeffoua@gmail.com>
2023-06-08 15:04:25 -0400

    routes

--- server/server.js
+++ server/server.js
@@ -8,14 +8,12 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('/01-Activities/')[1]
-  : cwd;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-
+const activity="zoosta"
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server for ${activity} running on port ${PORT}!`);
