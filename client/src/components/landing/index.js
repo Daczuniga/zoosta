2da389616145694a3f51191c40c4da479a88513a
Enrique Sanchez <esanchez8k@github.com>
2023-06-09 14:45:44 -0400

    added pictures

--- client/src/components/landing/index.js
+++ client/src/components/landing/index.js
@@ -1,4 +1,26 @@
-import React from "react"
-import login from "./login"
 
-const Landing
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Landing = () => {
+const history = useHistory();
+
+useEffect(() => {
+const videoDuration = 5000; // 5 seconds in milliseconds
+const timer = setTimeout(() => {
+history.push('/homepage');
+}, videoDuration);
+
+}, [history]);
+
+return (
+<div>
+<h1>Welcome to the Landing Page</h1>
+<video autoPlay controls>
+<source src={require('../../assets/zoosta_1.mp4').default} type="video/mp4" />
+</video>
+</div>
+);
+};
+
+export default Landing;
