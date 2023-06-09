92cb2df04b195a0f83c96f76b4db2d5a4ae1a126
Enrique Sanchez <esanchez8k@github.com>
2023-06-09 19:46:40 -0400

    merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
@@ -9,19 +8,6 @@ import Navbar from "./components/layout/NavBar";
 import Profile from "./components/profile";
 import Footer from "./components/layout/Footer";
 
-=======
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-<<<<<<< HEAD
-import Navbar from './components/Navbar';
->>>>>>> 8895c6a (create routes for pages, update client-pages and styles)
-=======
-import Navbar from './components/NavBar';
-import Footer from './components/Footer';
->>>>>>> efdc940 (fixed errors added footer)
 function App() {
   return (
    <div className="App">
@@ -29,29 +15,14 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/Home" element={<Home/>}/>
-<<<<<<< HEAD
           {/* <Route path="/Google" element={<Google/>}/> */}
           <Route path="/Login" element={<Login/>}/>
           <Route path="/SignUp" element={<SignUp/>}/>
           <Route path="/Post" element={<Post/>}/>
           <Route path="/Profile" element={<Profile/>}/>
         </Routes>
-<<<<<<< HEAD
-       
-        <Footer/>
-=======
-          <Route path="/Login" element={<Login/>}/>
-          <Route path="/SignUp" element={<SignUp/>}/>
-        </Routes>
-<<<<<<< HEAD
->>>>>>> 8895c6a (create routes for pages, update client-pages and styles)
-=======
-        <Footer/>
->>>>>>> efdc940 (fixed errors added footer)
-=======
->>>>>>> bf10324 (styled homepage)
-      </Router>
-   </div>
+       </Router>
+    </div>  
   );
 }
 
